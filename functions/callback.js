b122764fdfee5ddddf44c56d9c630c76d11615d1
ScioKNYT <48002129+ScioKNYT@users.noreply.github.com>
2026-01-05 16:59:34 +0530

    Implement GitHub OAuth callback function

---
+++ functions/callback.js
@@ -0,0 +1,23 @@
+export async function onRequestGet(context) {
+  const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = context.env;
+  const code = new URL(context.request.url).searchParams.get("code");
+
+  const response = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: { "content-type": "application/json", "accept": "application/json" },
+    body: JSON.stringify({ client_id: GITHUB_CLIENT_ID, client_secret: GITHUB_CLIENT_SECRET, code }),
+  });
+
+  const result = await response.json();
+  const script = `
+    <script>
+      const receiveMessage = (e) => {
+        if (e.data === "authorizing:github") {
+          window.opener.postMessage('authorization:github:success:${JSON.stringify({ token: result.access_token, provider: "github" })}', e.origin);
+        }
+      };
+      window.addEventListener("message", receiveMessage, false);
+      window.opener.postMessage("authorizing:github", "*");
+    </script>`;
+  return new Response(script, { headers: { "content-type": "text/html" } });
+}
