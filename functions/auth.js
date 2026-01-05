d030a5b32fdfddbbaac6e3b7997948e4a475332a
ScioKNYT <48002129+ScioKNYT@users.noreply.github.com>
2026-01-06 02:32:10 +0530

    Delete functions/auth.js

--- functions/auth.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const { GITHUB_CLIENT_ID } = context.env;
-  return new Response(null, {
-    status: 302,
-    headers: {
-      Location: `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=repo,user`,
-    },
-  });
-}
